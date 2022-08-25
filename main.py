211e5b8c33f1ab87f43259f3d379bd091f45fc44
2po4ill <ydevlicharov@bk.ru>
2022-08-25 09:16:27 +0300

    kwq

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
+
 import os
+import time
 
 
 def numbers(number):
     new = ''
-    for i in number:
-        if i in '.0123456789':
-            new += i
+    for m in number:
+        if m in '.0123456789':
+            new += m
     return new
+
+
+def verify(file):
+    for pdf in os.listdir('examples')[::-1]:
+        if pdf[:-4] in file and pdf != '':
+            return False
+    return True
 
 
 class Document:
@@ -51,87 +60,112 @@ class Values:
         return strtext
 
 
-previous = ''
-for i in os.listdir(r'primer otchetov'):
-    if '.pdf' in i and (previous[:-4] not in i or previous == '') and i[:-4] + '.txt' not in os.listdir('examples'):
-        reader = PdfReader('primer otchetov\\' + i)
-        if len(reader.pages) == 4:
-            document = reader.pages[0].extract_text()
-            listing = document.split('\n')
-            for j in listing:
-                if 'Engine Serial Number' in j and 'Page' not in j:
-                    serialnumber = j.split()[3]
-                elif 'Testbed' in j:
-                    testbed = j.split()[3]
-                elif 'Test Date' in j:
-                    testdate = j.split()[3]
-                elif 'Test Start' in j:
-                    teststart = j.split()[2]
-                elif 'TST_PAL' in j:
-                    testpal = j.split()[1]
-                elif 'Status:' in j:
-                    n = j.split().index('Status:')
-                    teststatus = j.split()[n+1]
+ticc = time.perf_counter()
+for i in os.listdir(r'tc 1'):
+    if '.pdf' in i:
+        tic = time.perf_counter()
+        reader = PdfReader('tc 1\\' + i)
+        writer = PdfWriter()
+        flag = verify(i)
+        if flag:
+            if len(reader.pages) < 8:
+                document = reader.pages[0].extract_text()
+                listing = document.split('\n')
+                serialnumber, testdate, teststart, teststatus, testpal, testbed = '', '', '', '', '', ''
+                p, fbrate, poil, opanlg, torque = '', '', '', '', ''
+                testratedhp, testtorquepeak, testlowidle2 = ('', '', '', ''), ('', '', ''), ('', '')
+                for j in listing:
+                    if 'Engine Serial Number' in j and 'Page' not in j:
+                        serialnumber = j.split()[3]
+                    elif 'Testbed' in j:
+                        testbed = j.split()[3]
+                    elif 'Test Date' in j:
+                        testdate = j.split()[3]
+                    elif 'Test Start' in j:
+                        teststart = j.split()[2]
+                    elif 'TST_PAL' in j:
+                        testpal = j.split()[1]
+                    elif 'Status:' in j:
+                        n = j.split().index('Status:')
+                        teststatus = j.split()[n + 1]
 
-            values = reader.pages[3].extract_text()
-            listing = values.split('\n')
-            for j in listing:
-                match j:
-                    case 'RATED_HP':
-                        index = listing.index(j) + 1
-                        head = listing[index]
-                        while head != 'TORQUE_PEAK':
-                            if 'P ' in head:
-                                p = head.split()[3]
-                            elif 'FB_RATE' in head:
-                                fbrate = head.split()[3]
-                            elif 'P_OIL' in head:
-                                poil = head.split()[3]
-                            elif 'OPA_ANLG' in head:
-                                opanlg = head.split()[3]
-                            index += 1
+                values = reader.pages[-1].extract_text()
+                listing = values.split('\n')
+                for j in listing:
+                    match j:
+                        case 'RATED_HP':
+                            index = listing.index(j) + 1
                             head = listing[index]
-                        testratedhp = (p, fbrate, poil, opanlg)
-                    case 'TORQUE_PEAK':
-                        index = listing.index(j) + 1
-                        head = listing[index]
-                        while head != 'LOW_IDLE_2':
-                            if 'TORQUE' in head:
-                                torque = head.split()[3]
-                            elif 'FB_RATE' in head:
-                                fbrate = head.split()[3]
-                            elif 'OPA_ANLG' in head:
-                                opanlg = head.split()[3]
-                            index += 1
+                            while head != 'TORQUE_PEAK':
+                                if 'P ' in head:
+                                    p = head.split()[3]
+                                elif 'FB_RATE' in head:
+                                    fbrate = head.split()[3]
+                                elif 'P_OIL' in head:
+                                    poil = head.split()[3]
+                                elif 'OPA_ANLG' in head:
+                                    if len(head.split()) < 4:
+                                        opanlg = numbers(head.split()[-1])
+                                    else:
+                                        opanlg = head.split()[3]
+
+                                index += 1
+                                if index != len(listing):
+                                    head = listing[index]
+                                else:
+                                    break
+                            testratedhp = (p, fbrate, poil, opanlg)
+                        case 'TORQUE_PEAK':
+                            index = listing.index(j) + 1
                             head = listing[index]
-                        testtorquepeak = (torque, fbrate, opanlg)
-                    case 'LOW_IDLE_2':
-                        index = listing.index(j) + 1
-                        head = listing[index]
-                        while head != 'Test Report':
-                            if 'P_OIL' in head:
-                                poil = head.split()[3]
-                            elif 'OPA_ANLG' in head:
-                                opanlg = head.split()[2]
-                            index += 1
+                            while head != 'LOW_IDLE_2' or 'TORQUE_PEAK' in head:
+                                if 'TORQUE' in head and 'PEAK' not in head:
+                                    torque = head.split()[3]
+                                elif 'FB_RATE' in head:
+                                    fbrate = head.split()[3]
+                                elif 'OPA_ANLG' in head:
+                                    try:
+                                        opanlg = head.split()[3]
+                                    except Exception as e:
+                                        opanlg = head.split()[2]
+                                index += 1
+                                if index != len(listing):
+                                    head = listing[index]
+                                else:
+                                    break
+                            testtorquepeak = (torque, fbrate, opanlg)
+                        case 'LOW_IDLE_2':
+                            index = listing.index(j) + 1
                             head = listing[index]
-                        testlowidle2 = (poil, numbers(opanlg))
-            testdata = Document(serialnumber, testdate, teststart, teststatus, testpal, testbed)
-            testvalues = Values(testratedhp, testtorquepeak, testlowidle2)
-            extention = testdata.serialnumber + ';' + testdata.bed + \
-                        ';' + testdata.pal + ';' + testdata.date + \
-                        ';' + testdata.start + ';' + testdata.status + \
-                        ';' + testvalues.ratedhp[0] + ';' + testvalues.ratedhp[1] + \
-                        ';' + testvalues.ratedhp[2] + ';' + testvalues.ratedhp[3] + \
-                        ';' + testvalues.torquepeak[0] + ';' + testvalues.torquepeak[1] + \
-                        ';' + testvalues.torquepeak[2] + ';' + testvalues.lowidle2[0] + \
-                        ';' + testvalues.lowidle2[1]+';'
-            name = numbers(testdata.date) + '_' + numbers(testdata.start) + '_' + \
-                   testdata.serialnumber + '_' + testdata.status + '.txt'
-            write = open('examples\\' + name, 'w')
-            write.write(extention + '\n \n' + str(testdata) + '\n \n' + str(testvalues))
-    previous = i
-
-
-
-
+                            while head != 'Test Report':
+                                if 'P_OIL' in head:
+                                    poil = head.split()[3]
+                                elif 'OPA_ANLG' in head:
+                                    opanlg = head.split()[2]
+                                index += 1
+                                head = listing[index]
+                            testlowidle2 = (poil, numbers(opanlg))
+                testdata = Document(serialnumber, testdate, teststart, teststatus, testpal, testbed)
+                testvalues = Values(testratedhp, testtorquepeak, testlowidle2)
+                extention = testdata.serialnumber + ';' + testdata.bed + \
+                            ';' + testdata.pal + ';' + testdata.date + \
+                            ';' + testdata.start + ';' + testdata.status + \
+                            ';' + testvalues.ratedhp[0] + ';' + testvalues.ratedhp[1] + \
+                            ';' + testvalues.ratedhp[2] + ';' + testvalues.ratedhp[3] + \
+                            ';' + testvalues.torquepeak[0] + ';' + testvalues.torquepeak[1] + \
+                            ';' + testvalues.torquepeak[2] + ';' + testvalues.lowidle2[0] + \
+                            ';' + testvalues.lowidle2[1] + ';'
+                name = numbers(testdata.date) + '_' + numbers(testdata.start) + '_' + \
+                       testdata.serialnumber + '_' + testdata.status
+                write = open('examples\\' + name + '.txt', 'w')
+                write.write(extention + '\n \n' + str(testdata) + '\n \n' + str(testvalues))
+        for x in reader.pages:
+            writer.add_page(x)
+        writer.write('archive\\' + i)
+        if os.path.exists('archive\\' + i) and os.stat('archive\\' + i).st_size > 1000:
+            os.remove('tc 1\\' + i)
+        tac = time.perf_counter()
+        print(f"Вычисление заняло {tac - tic:0.4f} секунд")
+tacc = time.perf_counter()
+print(f"Вычисление заняло {tacc - ticc:0.4f} секунд для " + str(len(os.listdir('tc 1'))) + ' файлов')
+print(len(os.listdir('archive')))
